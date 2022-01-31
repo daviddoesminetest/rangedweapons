3f7df33cc1ce8424d6266d4e99ea6bc820e81cf8
ranguli <hello@joshmurphy.ca>
2022-01-31 18:26:15 -0330

    Remove redundant if statements in armor.lua

--- armor.lua
+++ armor.lua
@@ -1,9 +1,5 @@
 if minetest.global_exists("armor") and armor.attributes then
 	table.insert(armor.attributes, "bullet_res")
-end
-if minetest.global_exists("armor") and armor.attributes then
 	table.insert(armor.attributes, "ammo_save")
-end
-if minetest.global_exists("armor") and armor.attributes then
 	table.insert(armor.attributes, "ranged_dmg")
 end
